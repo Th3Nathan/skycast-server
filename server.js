a18b7a8cb1ae368a97ebfd3b04079c0223cb0f0f
Nathan Vass <nathanevass@gmail.com>
2017-12-14 00:17:04 -0500

    different headers

--- server.js
+++ server.js
@@ -20,7 +20,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors('*'));
+app.use(function (req, res, next) {
+    
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://nathanskycast.herokuapp.com');
+    
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
+    });
 // app.use((req, res, next) =>{
 // res.header ('Access-Control-Allow-Origin', 'http://localhost:3000, http://nathanskycast.herokuapp.com')
 // res.header ('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept')
