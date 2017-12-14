06398aa565072f6dcc3c82ee4a641545a23fb711
Nathan Vass <nathanevass@gmail.com>
2017-12-13 23:46:07 -0500

    add access control origin for heroku

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) =>{
-res.header ('Access-Control-Allow-Origin', 'http://localhost:3000 ')
+res.header ('Access-Control-Allow-Origin', 'http://localhost:3000, http://nathanskycast.herokuapp.com')
 res.header ('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept')
 res.header ('Access-Control-Allow-Credentials', true)
 res.header ('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
